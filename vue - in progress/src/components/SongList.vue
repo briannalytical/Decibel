9e7e036ee72651e2483066e0773ce512a4e66a21
Jennifer Brown <jenkbrown2@gmail.com>
2023-04-11 07:42:21 -0400

    hotel

---
+++ vue - in progress/src/components/SongList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="songlist">
+      <div v-for="currentSong in songs" v-bind:key="currentSong.id">
+      <SongListItem />
+      </div>
+  </div>
+</template>
+
+<script>
+
+import SongListItem from '@/components/SongListItem'
+import SongListService from '@/services/SongListService'
+
+export default {
+    name: 'song-list',
+    components: {
+        SongListItem
+    },
+    data() {
+        return {
+            songs: []
+        }
+    },
+    created() {
+        SongListService.getAllSongs()
+        .then(response => {
+            this.song = response.data;
+        })
+        .catch(err => console.error(err) );
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue - in progress/src/views/Songs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <SongDetails />
+  </div>
+</template>
+
+<script>
+
+import SongDetails from '@/components/SongDetails'
+
+export default {
+    components: {
+        SongDetails
+    }
+
+}
+</script>
+
+<style>
+
+</style>

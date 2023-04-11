1f9dd25643a581ce4a6b7d7ed23d4fb43b66e18b
Travis Nyland <travis.nyland@gmail.com>
2023-04-11 17:06:08 -0400

    back button done

---
+++ vue - in progress/src/components/BackButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="back-button" @click="goBack">
+      &#8592; Back
+  </button>
+</template>
+
+<script>
+export default {
+    methods: {
+        goBack() {
+            this.$router.go(-1);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.back-button {
+    background-color: #f8f9fa;
+    border: none;
+    color: #077bff;
+    padding: 8px 16px;
+    font-size: 16px;
+    cursor: pointer;
+    text-decoration: none;
+    border-radius: 4px;
+    transition: background-color 0.3s, color 0.3s;
+}
+
+.back-button:hover {
+    background-color: #077bff;
+    color: #ffffff;
+}
+</style>
--- vue - in progress/src/views/Songs.vue
+++ vue - in progress/src/views/Songs.vue
@@ -1,7 +1,11 @@
 <template>
+
   <div>
       <SongDetails />
       <SongList />
+      <div class="back-button-container">
+    <back-button></back-button>
+        </div>
   </div>
 </template>
 
@@ -9,16 +13,24 @@
 
 import SongDetails from '@/components/SongDetails'
 import SongList from '@/components/SongList'
+import BackButton from '../components/BackButton.vue'
 
 export default {
     components: {
         SongDetails,
-        SongList
+        SongList,
+        BackButton
     }
 
 }
 </script>
 
-<style>
+<style scoped>
+.back-button-container {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    z-index: 10;
+}
 
 </style>

e20f1f549936aa480b4b3ea65dcf159d57de5eee
William Gainsborough <wgainsborough37@gmail.com>
2023-04-12 10:18:30 -0400

    Mob Playlist Commit

--- vue - in progress/src/components/SongDetails.vue
+++ vue - in progress/src/components/SongDetails.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="songDetail">
-    <h2>Moods {{ mood.id }}</h2>
+    
     <Mood v-bind:mood="mood" />
-    <h2>Songs {{ song.title }}</h2>
+    <h2>Playlist {{ song.title }}</h2>
     <SongList v-bind:song="song" />
   </div>
 </template>
--- vue - in progress/src/components/SongList.vue
+++ vue - in progress/src/components/SongList.vue
@@ -3,13 +3,27 @@
     <div v-for="currentMood in mood" v-bind:key="currentMood.id">
       <router-link
         v-bind:to="{ name: 'mood' }">
-        {{ currentMood.mood }}
         <Mood v-bind:mood="currentMood"/>
       </router-link>
       <div class="songlist">
         <div v-for="currentSong in songs" v-bind:key="currentSong.id">
-          {{ currentSong.title }}
-          <SongListItem />
+                   <SongListItem />
+              <div id="playlist">
+        <table id="song-table">
+            <tr class="column">
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Mood</th>
+                <th>Genre</th>
+              </tr>
+              <tr>
+                <td>{{ currentSong.title }}</td>
+                <td>{{ currentSong.artist }}</td>
+                <td>{{ currentSong.mood }}</td>
+                <td>{{ currentSong.genre }}</td>
+              </tr>
+        </table>
+        </div>
         </div>
       </div>
     </div>
@@ -59,4 +73,30 @@ export default {
 </script>
 
 <style>
+
+
+table {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+}
+
+table,
+th,
+td {
+    border: 1px solid;
+}
+
+th,
+td {
+    padding: 15px;
+    text-align: left;
+}
+
+th {
+    background-color: #fffb001e;
+    color: rgb(0, 0, 0);
+}
+
 </style>

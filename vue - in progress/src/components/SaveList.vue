90f75da0276bc226f38b3ebff2b839efe5113a7b
William Gainsborough <wgainsborough37@gmail.com>
2023-04-13 13:59:43 -0400

    Sprint 2 Day 1 Commit 2

--- vue - in progress/src/components/SaveList.vue
+++ vue - in progress/src/components/SaveList.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
- <div>
-   <p v-for="currentPlaylist in savedPlaylist" v-bind:key="currentPlaylist.playlistId"></p>
-   <p>{{currentPlaylist.playlistId}}</p>
-   <p>{{currentPlaylist.playlistName}}</p>
+ <div v-for="currentPlaylist in savedPlaylist" v-bind:key="currentPlaylist.playlistId">
+     <button class="save-button" @click="savePlaylist">
+     &#8592; Save Playlist  
+  </button>
  </div>
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
     }
   },
 
-    name: 'save-list',
+    name: 'savelist',
     computed: {
 
       savedPlaylist() {
@@ -29,16 +29,45 @@ export default {
       }
     },
      created() {
-    SongListService.getPlaylistById()
+       const playlistId = this.$route.params.id;
+
+    SongListService.getPlaylistById(playlistId)
       .then((response) => {
         this.playlist = response.data;
       })
       .catch((err) => console.error(err));
-     }
+     },
+
+     methods: {
+    savePlaylist() {
+      const playlistId = this.$route.params.id;
+        this.$router.push({
+            name:"savelist", 
+            params: {
+              id: playlistId
+            }
+        })
+    }
+    }
 
 }
 </script>
 
 <style>
+.save-button {
+    background-color: #f8f9fa;
+    border: none;
+    color: #077bff;
+    /* padding: 8px 16px; */
+    font-size: 16px;
+    cursor: pointer;
+    /* text-decoration: none; */
+    border-radius: 4px;
+    transition: background-color 0.3s, color 0.3s;
+}
 
+.save-button:hover {
+    background-color: #077bff;
+    color: #ffffff;
+}
 </style>

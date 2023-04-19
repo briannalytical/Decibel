7104063413ede0aa9893ea2c8fd997ca03ae16f9
Jennifer Brown <jenkbrown2@gmail.com>
2023-04-19 12:43:23 -0400

    4.19.23

--- vue - in progress/src/views/Songs.vue
+++ vue - in progress/src/views/Songs.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
-    <SongDetails />
+   
     <div class="back-button-container">
       <back-button></back-button>
+     
     
     </div>
   </div>
 </template>
 
 <script>
-import SongDetails from "@/components/SongDetails";
+
 import BackButton from "../components/BackButton.vue";
 
 
 export default {
   components: {
-    SongDetails,
+  
     BackButton,
    
   },
